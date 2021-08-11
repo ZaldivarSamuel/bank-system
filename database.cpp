2ff3bcb216d13e9a1ef65bf1e085dff6ec1bc0d4
Samuel Zaldivar <samzaldivar98@gmail.com>
2021-08-11 10:27:28 -0400

    Creates the Accounts table

--- database.cpp
+++ database.cpp
@@ -1,7 +1,45 @@
 #include "database.h"
 #include <iostream>
-using namespace std;
+
+static int callback(void *data, int argc, char **argv, char **azColName){
+   int i;
+   fprintf(stderr, "%s: ", (const char*)data);
+   
+   for(i = 0; i<argc; i++){
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   
+   printf("\n");
+   return 0;
+}
+
 
 Database::Database(){
-    cout << "Creating Database" << endl;
+    rc = sqlite3_open("bank.db", &db);
+
+    if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(1);
+   } else {
+      fprintf(stderr, "Opened database successfully\n");
+      createTables();
+   }
 }
+
+void Database::createTables(){
+    std::cout << "Creating Tables" << std::endl;
+
+    char const *sql = "CREATE TABLE Accounts("  \
+      "ID INT PRIMARY KEY     NOT NULL," \
+      "Amount           DOUBLE    NOT NULL );";
+
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   } else {
+      fprintf(stdout, "Table created successfully\n");
+   }
+}
+
--- database.h
+++ database.h
@@ -1,13 +1,15 @@
 #include <sqlite3.h>
 
-using namespace std;
-
 class Database{
 
 public:
     sqlite3 *db;
     int rc;
 
-
     Database();
+
+private:
+     char *zErrMsg = 0;
+    void createTables();
+   // int callback(void *data, int argc, char **argv, char **azColName);
 };
